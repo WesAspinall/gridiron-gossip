663b3ca86da028ef623d8394b176c7c3390b7164
Wesley Aspinall <wra427@gmail.com>
2016-11-02 14:26:48 -0400

    top players card, top crimes card

---
+++ src/app/components/crimes/top-crimes/crimesCard.component.js
@@ -0,0 +1,7 @@
+var crimesCard = {
+  templateUrl: './crimes-card.html'
+}
+
+angular
+  .module('components.crimes')
+  .component('crimesCard', crimesCard);
---
+++ src/app/components/players/top-players/topPlayersCard.component.js
@@ -0,0 +1,7 @@
+var topPlayersCard = {
+  templateUrl: './top-players-card.html'
+}
+
+angular
+  .module('components.players')
+  .component('topPlayersCard', topPlayersCard);
